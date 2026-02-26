9e682586cd01eb90a7a2bdf3c648ee823acbe8d9
Sravana Kumar <srkumar@consilio.com>
2026-02-26 08:31:06 +0530

    testing rollback mechanism

--- app/service.py
+++ app/service.py
@@ -2,7 +2,7 @@ def welcome_message():
     return f"Welcome!"
 
 def health_status():
-    return {"status": "DOWN"}, 500
+    return {"status": "healthy"}
 
 def greetings():
     return "I'm good, how about you?"

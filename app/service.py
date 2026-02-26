d342ab6ed619beb288c70102de2e52cfe7fd4780
Sravana Kumar <srkumar@consilio.com>
2026-02-26 08:36:18 +0530

    testing rollback mechanism

--- app/service.py
+++ app/service.py
@@ -1,8 +1,14 @@
+
+import os
+
+
 def welcome_message():
     return f"Welcome!"
 
 def health_status():
-    return {"status": "healthy"}
+    if os.environ.get("FAIL_HEALTH") == "true":
+        return {"status": "DOWN"}, 500
+    return {"status": "UP"}
 
 def greetings():
     return "I'm good, how about you?"
